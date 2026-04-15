bce86ec4fedfdf75a24cb9e361a7f46640cd464f
NIKHIL YADAV <nikhilyadav835402@gmail.com>
2026-04-15 11:22:15 +0530

    firstcommit

---
+++ first.js
@@ -0,0 +1,15 @@
+// Create element through JS
+
+
+// const header1 = document.createElement('h1');
+// header1.innerText = "Hello Coder Army";
+// header1.style.backgroundColor = "blue";
+// header1.style.fontSize = "30px";
+// header1.style.color = "white";
+
+
+// const header2 = document.createElement('h2');
+// header2.innerText = "Kaise ho aap sab log";
+// header2.style.backgroundColor = "black";
+// header2.style.fontSize = "25px";
+// header2.style.color = "white";
